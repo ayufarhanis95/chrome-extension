3ed19b9f40e675e4b33c4eea0440d1f12e77ff11
ayufarhanis95 <144410448+ayufarhanis95@users.noreply.github.com>
2023-11-29 00:01:47 +0800

    Added javascript for adding link and deleting link

--- index.js
+++ index.js
@@ -2,21 +2,39 @@ let myLeads = []
 const linkBtn = document.getElementById("link-btn")
 const inputEl = document.getElementById("input-el")
 const savedLinks = document.getElementById("saved-links")
+const deleteBtn = document.getElementById("delete-btn")
+const addBtn = document.getElementById("tab-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
 
 linkBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     inputEl.value = ""
     render(myLeads)
-    
-
-})
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+ })
 
 function render(x) {
+    let listItems = ""
     for (let i = 0; i < x.length; i = i + 1) {
-        savedLinks.innerHTML += `<li><a href='{x[i]}' target='_blank'> ${x[i]} </a></li>`
+        listItems += `<li><a href='${x[i]}' target='_blank'>${x[i]}</a></li>`
     }
+    savedLinks.innerHTML = listItems
+    
     
 }
+
+deleteBtn.addEventListener("click",function(){
+    savedLinks.innerHTML = " "
+    localStorage.clear()
+    myLeads = []
+})
+
 
 
 
